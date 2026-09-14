f5f456c57beb3483521b1719155e000e0fd0ea9c
Mawkli <aymanemj@proton.me>
2026-09-14 16:16:29 +0100

    Consuming API to get vid IDs
    saving the vid IDs to a temporary data/vid_id.txt file,
    to use later when getting vid details.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,67 @@
-import requests
 import os
+import pprint as pr
+
+import requests as rqst
+from dotenv import load_dotenv
+
+# Loading .env variables
+load_dotenv(dotenv_path="../.env")
+BASE_URL = os.getenv("BASE_URL")
+API_KEY = os.getenv("API_KEY")
+CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+
+#Temp JSON file for video IDs
+VID_ID_JSON = open("../data/vid_id.txt","w")
+
+
+# Params for each request
+get_creator_playlist = {
+    "key" : API_KEY,
+    "forHandle" : CHANNEL_HANDLE,
+    "part" : "contentDetails"
+}
+
+get_playlist_details = {
+    "key": API_KEY,
+    "part": "contentDetails",
+    "playlistId": None,
+    "maxResults": 50,
+}
 
 
 
+
+def getCreatorPlaylist(params):
+    r = rqst.get(BASE_URL + "/channels", params)
+    data = r.json()
+    uploads_playlist_id = data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+    return uploads_playlist_id
+
+def getPlaylistDetails(playlist_id, params):
+    params["playlistId"] = playlist_id
+    # print(params)
+    r = rqst.get(BASE_URL + "/playlistItems", params)
+    return r.json()
+
+def loadVidIdToJson(json):
+    for i in json:
+        VID_ID_JSON.write(f"{i["contentDetails"]["videoId"]}\n")
+    
+
+PLAYLIST_ID = getCreatorPlaylist(get_creator_playlist)
+
+
+
+while True:
+    response = getPlaylistDetails(PLAYLIST_ID, get_playlist_details)
+    
+    loadVidIdToJson(response["items"])
+    # pr.pprint(response)
+    if "nextPageToken" not in response:
+        break
+    else:
+        get_playlist_details["pageToken"] = response["nextPageToken"]
+
+
+    
+VID_ID_JSON.close()
